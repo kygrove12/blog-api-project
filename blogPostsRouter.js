f7dadfc6fc60093f8e181347a015f18bfe158216
kygrove12 <kygrove12@gmail.com>
2018-08-25 16:54:06 -0700

    Added server.js and blogPostsRouter.js

---
+++ blogPostsRouter.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+
+const { BlogPosts } = require("./models");
+
+BlogPosts.create("First Ever Post!", "This is the body of our first example blog post! So exciting!", "Author's Name");
+BlogPosts.create("Another One", "And we're back! Can't have just one post on this fake blog.", "Author's Name");
+
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.post("/", (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(item);
+});
+
+router.put("/:id", (req, res) => {
+  const requiredFields = ["id", "title", "content", "author", "publishDate"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${
+      req.params.id
+    }) and request body id ``(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
+
+
+router.delete("/:id", (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = router;
